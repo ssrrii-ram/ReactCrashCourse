f586f87e15e4a936df1d901425441b0db44a853a
sri <sriswethamani1998@gmail.com>
2024-11-08 19:26:00 +0530

    fetched live data and populated cards

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -17,11 +17,20 @@ const Body = () => {
   };
 
   useEffect(() => {
-    setTimeout(() => {
-      setListOfRestaurants(resList);
-      setAllRestaurants(resList);
-    }, 2000);
+    getRestaurants();
   }, []);
+
+  async function getRestaurants() {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+
+    const restaurants =
+      json.data.cards[1].card?.card?.gridElements?.infoWithStyle?.restaurants;
+    setAllRestaurants(restaurants);
+    setListOfRestaurants(restaurants);
+  }
 
   if (listOfRestaurants.length === 0) {
     return <Shimmer />;
